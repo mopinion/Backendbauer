78ec68d7d4e9ab5309e8f1dffbe9ec3dbac5a48d
Floris Snuif <floris@mopinionlabs.com>
2016-03-17 15:50:00 +0100

    stop both

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 git pull origin master
 sudo docker stop test
 sudo docker rm test
+sudo docker stop backendbauer
+sudo docker rm backendbauer
 sudo docker build -t backendbauer .
 sudo docker run -ti -p 81:81 --name test backendbauer
