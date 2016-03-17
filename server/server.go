be9fddf798ac5c631530283a354d65e583d54506
Floris Snuif <floris@mopinionlabs.com>
2016-03-17 15:59:35 +0100

    never mind

--- server/server.go
+++ server/server.go
@@ -203,8 +203,7 @@ func password(user, realm string) string {
 
 // load json
 func (bb *Backendbauer) settings() jsonobject {
-	fmt.Println("bb.path:",bb.path)
-	file, err := ioutil.ReadFile(bb.path + "config.json")
+	file, err := ioutil.ReadFile("/var/www/backendbauer/server/config.json")
 	if err != nil {
 		panic(err)
 	}
